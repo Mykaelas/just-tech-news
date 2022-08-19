645d5003ef9fd4ca91a5a1f0443fb94d2fccd150
Mykaela Saenz <mickycore@gmail.com>
2022-08-19 13:53:43 -0500

    added in a post.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
-const User = require('./User');
-module.exports = { User };
+const User = require("./User");
+const Post = require("./Post")
+module.exports = { User, Post };
 
 //All this file is responsible for right now is importing the User model and exporting an object with it as a property 
